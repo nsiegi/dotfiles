dc0ff26e55d840bcd01d981e58b74710e8d521b4
Nsiegi <nsiegi@users.noreply.github.com>
2024-03-21 19:36:39 +0200

    Configured nvim settings

--- .config/nvim/lua/default/set.lua
+++ .config/nvim/lua/default/set.lua
@@ -1,5 +1,5 @@
 require("catppuccin").setup({ flavour = "macchiato", transparent_background = false })
-vim.cmd.colorscheme('catppuccin')
+vim.cmd.colorscheme("catppuccin")
 
 -- disable netrw at the very start
 vim.g.loaded_netrw = 1
@@ -26,23 +26,24 @@ vim.opt.signcolumn = "yes"
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
-vim.opt.fillchars = 'eob: '
-vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+vim.opt.fillchars = "eob: "
+vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
 
 -- Decrease update time
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 
 -- Sets how neovim will display certain whitespace in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
+--  See `:help "list"`
+--  and `:help "listchars"`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
+vim.opt.virtualedit = "block"
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
